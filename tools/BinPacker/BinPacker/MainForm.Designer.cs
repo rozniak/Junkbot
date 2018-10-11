52d396ede5dc441ec8557b108944fb52e4a47592
Rory Fewell <roryfewell@hotmail.com>
2018-10-11 21:21:17 +0100

    Add BinPacker sprite packing tool to repo

---
+++ tools/BinPacker/BinPacker/MainForm.Designer.cs
@@ -0,0 +1,66 @@
+﻿namespace BinPacker
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.RenderTarget = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.RenderTarget)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // RenderTarget
+            // 
+            this.RenderTarget.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.RenderTarget.Location = new System.Drawing.Point(0, 0);
+            this.RenderTarget.Name = "RenderTarget";
+            this.RenderTarget.Size = new System.Drawing.Size(512, 512);
+            this.RenderTarget.TabIndex = 0;
+            this.RenderTarget.TabStop = false;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(512, 512);
+            this.Controls.Add(this.RenderTarget);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Bin Packing Test";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.RenderTarget)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox RenderTarget;
+    }
+}
+
---
+++ tools/BinPacker/BinPacker/MainForm.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BinPacker
+{
+    public partial class MainForm : Form
+    {
+        const string TexturePath = @"C:\Users\Rory\Source\Repos\Junkbot\Junkbot\Content";
+
+        private Node RootNode;
+
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            RenderTarget.BackgroundImage = new Bitmap(RenderTarget.Width, RenderTarget.Height);
+
+            RootNode = new Node(0, 0, RenderTarget.Width, RenderTarget.Height, null, null, null);
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            string[] textureFiles = Directory.GetFiles(TexturePath);
+            
+
+            foreach (string filename in textureFiles)
+            {
+                using (var tex = (Bitmap)Image.FromFile(filename))
+                {
+                    LoadToCache(tex);
+                }
+            }
+        }
+
+
+        private void LoadToCache(Bitmap img)
+        {
+            Node node = RootNode.Insert(RootNode, new Rectangle(Point.Empty, img.Size));
+
+            if (node != null)
+            {
+                using (var g = Graphics.FromImage(RenderTarget.BackgroundImage))
+                {
+                    g.DrawImage(img, node.Rect);
+                }
+
+                node.LeaveName = "117";
+            }
+            else
+            {
+                MessageBox.Show("Out of room!");
+            }
+        }
+    }
+
+
+    internal class Node
+    {
+        public Node LeftChild;
+        public Node RightChild;
+        public Rectangle Rect;
+        public String LeaveName;
+
+
+        public Node(int x, int y, int width, int height, Node leftChild, Node rightChild, string leaveName)
+        {
+            Rect = new Rectangle(x, y, width, height);
+            LeftChild = leftChild;
+            RightChild = rightChild;
+            LeaveName = leaveName;
+        }
+
+        public Node()
+        {
+            Rect = new Rectangle();
+        }
+
+
+        public Node Insert(Node node, Rectangle rect)
+        {
+            if (node.LeaveName == null && node.LeftChild != null && node.RightChild != null)
+            {
+                Node newNode = null;
+
+                newNode = Insert(node.LeftChild, rect);
+
+                if (newNode == null) newNode = Insert(node.RightChild, rect);
+
+                return newNode;
+            }
+            else
+            {
+                if (node.LeaveName != null) return null;
+
+                if (node.Rect.Width == rect.Width && node.Rect.Height == rect.Height) return node;
+
+                if (node.Rect.Width < rect.Width || node.Rect.Height < rect.Height) return null;
+
+                node.LeftChild = new Node();
+                node.RightChild = new Node();
+
+                int deltaWidth = node.Rect.Width - rect.Width;
+                int deltaHeight = node.Rect.Height - rect.Height;
+
+                if (deltaWidth > deltaHeight)
+                {
+                    node.LeftChild.Rect = new Rectangle(node.Rect.X, node.Rect.Y, rect.Width, node.Rect.Height);
+                    node.RightChild.Rect = new Rectangle(node.Rect.X + rect.Width, node.Rect.Y, node.Rect.Width - rect.Width, node.Rect.Height);
+                }
+                else
+                {
+                    node.LeftChild.Rect = new Rectangle(node.Rect.X, node.Rect.Y, node.Rect.Width, rect.Height);
+                    node.RightChild.Rect = new Rectangle(node.Rect.X, node.Rect.Y + rect.Height, node.Rect.Width, node.Rect.Height - rect.Height);
+                }
+
+                return Insert(node.LeftChild, rect);
+            }
+        }
+    }
+}
