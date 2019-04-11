c62cfb348ed827727b9215201b66ffa1b15e814f
Rory Fewell <roryfewell@hotmail.com>
2019-04-11 20:08:58 +0100

    Start work on abstracting out the model of the game engine to a separate library

---
+++ Rzxe/GameEntryPoint.cs
@@ -0,0 +1,54 @@
+﻿using Oddmatics.Rzxe.Game;
+using Oddmatics.Rzxe.Windowing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Oddmatics.Rzxe
+{
+    public sealed class GameEntryPoint
+    {
+        private IGameEngine _GameEngine;
+        public IGameEngine GameEngine
+        {
+            get { return _GameEngine; }
+            set
+            {
+                if (IsRunning)
+                {
+                    throw new InvalidOperationException(
+                        "Cannot set game engine when the game is running."
+                        );
+                }
+                else
+                {
+                    _GameEngine = value;
+                }
+            }
+        }
+
+
+        public bool IsRunning { get; private set; }
+
+        private IWindowManager WindowManager { get; set; }
+
+
+        public void Initialize()
+        {
+            WindowManager.Initialize();
+        }
+
+        public void Run()
+        {
+            if (WindowManager == null || !WindowManager.Ready)
+            {
+                throw new InvalidOperationException("No window manager initialized.");
+            }
+
+            // Enter the main game loop
+            //
+
+        }
+    }
+}
---
+++ Rzxe/Windowing/IWindowManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Oddmatics.Rzxe.Windowing
+{
+    public interface IWindowManager
+    {
+        bool Ready { get; }
+
+
+        void Initialize();
+    }
+}
