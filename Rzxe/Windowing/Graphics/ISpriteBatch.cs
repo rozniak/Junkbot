6950803c61e534415929d1d04f25b41c0850fcb3
Rory Fewell <roryfewell@hotmail.com>
2019-04-15 20:45:59 +0100

    Add ISpriteBatch interface

---
+++ Rzxe/Windowing/Graphics/ISpriteBatch.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+
+namespace Oddmatics.Rzxe.Windowing.Graphics
+{
+    public interface ISpriteBatch
+    {
+        void Draw(string spriteName, Rectangle rect);
+
+        void Finish();
+    }
+}
